0f451dbce4ec9556f4b93af58f88b3386529222d
Thomas Roovers <troovers@e-sites.nl>
2023-02-23 15:17:36 +0100

    Added a specific LocationPermissionStatus and export status enums

--- lib/permissions.dart
+++ lib/permissions.dart
@@ -1,32 +1,36 @@
-import 'permissions_platform_interface.dart';
-import 'permission_status.dart';
+library permissions;
+
+import 'src/permissions_platform_interface.dart';
+import 'src/permission_status.dart';
+
+export 'src/permission_status.dart' show PermissionStatus, LocationPermissionStatus;
 
 class Permissions {
-  Future<PermissionStatus> getLocationPermissionStatus() {
+  Future<LocationPermissionStatus> getLocationPermissionStatus() {
     return PermissionsPlatform.instance.getLocationPermissionStatus().then((value) => _getStatusFromString(value));
   }
 
-  Future<PermissionStatus> requestLocationPermission(bool always) {
+  Future<LocationPermissionStatus> requestLocationPermission(bool always) {
     return PermissionsPlatform.instance.requestLocationPermission(always).then((value) => _getStatusFromString(value));
   }
 
-  PermissionStatus _getStatusFromString(String? string) {
+  LocationPermissionStatus _getStatusFromString(String? string) {
     switch (string) {
       case "authorizedAlways":
-        return PermissionStatus.authorizedAlways;
+        return LocationPermissionStatus.authorizedAlways;
 
       case "authorizedWhenInUse":
-        return PermissionStatus.authorizedWhenInUse;
+        return LocationPermissionStatus.authorizedWhenInUse;
 
       case "denied":
       case "restricted":
-        return PermissionStatus.denied;
+        return LocationPermissionStatus.denied;
 
       case "pending":
-        return PermissionStatus.pending;
+        return LocationPermissionStatus.pending;
 
       default:
-        return PermissionStatus.unknown;
+        return LocationPermissionStatus.unknown;
     }
   }
 }
---
+++ lib/src/permission_status.dart
@@ -0,0 +1,3 @@
+enum PermissionStatus { pending, authorized, denied, unknown }
+
+enum LocationPermissionStatus { pending, authorizedAlways, authorizedWhenInUse, denied, unknown }
